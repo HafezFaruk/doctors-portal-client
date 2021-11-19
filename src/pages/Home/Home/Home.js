a52c3bddf31dfe94807937ae7a904fbbcb88fccb
HafezFaruk <hmomarfaruk1454@gmail.com>
2021-11-19 18:09:08 +0600

    fill upload

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from '../../Shared/Navigation/Navigation';
 import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
+import Doctors from '../Doctors/Doctors.js';
 import Footer from '../Footer/Footer';
 import Services from '../Services/Services';
 import Banner from './Banner/Banner';
@@ -12,6 +13,7 @@ const Home = () => {
             <Banner></Banner>
             <Services></Services>
             <AppointmentBanner></AppointmentBanner>
+            <Doctors></Doctors>
             <Footer></Footer>
         </div>
     );
